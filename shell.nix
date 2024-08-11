7dd3438c5ef7b3be6d7ab8b23c43f35de5758a5a
Nadeŭka <nadevko@riseup.net>
2024-08-12 02:55:18 +0300

    Run and debug code in vscode

--- shell.nix
+++ shell.nix
@@ -1,18 +1,8 @@
-with import <nixpkgs> { };
-mkShell rec {
-  nativeBuildInputs = [
-    meson
-    ninja
-    pkg-config
-    cmake-format
-    clang-tools
-    desktop-file-utils
-    plantuml
-    cambalache
-    (callPackage
-      "${fetchGit "https://github.com/bkryza/clang-uml"}/packaging/nix" { })
-  ];
-  buildInputs = [ gtkmm4.dev libadwaita gettext opencv ];
-  NIX_LD_LIBRARY_PATH = lib.makeLibraryPath ([ stdenv.cc.cc ] ++ buildInputs);
-  NIX_LD = "${stdenv.cc.libc_bin}/bin/ld.so";
+{
+  pkgs ? import <nixpkgs> { },
+}:
+with pkgs;
+mkShell {
+  name = "FAnP-1";
+  packages = [ ];
 }
